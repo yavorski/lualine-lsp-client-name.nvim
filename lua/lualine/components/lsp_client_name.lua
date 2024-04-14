5d2e3789bd4cce91681221d60cf6931abf32822b
yavorski <yavorski.wrx@gmail.com>
2024-04-14 19:57:32 +0300

    show all clients

--- lua/lualine/components/lsp_client_name.lua
+++ lua/lualine/components/lsp_client_name.lua
@@ -1,11 +1,23 @@
 local M = require("lualine.component"):extend()
 
+M.replacements = {
+  ["_language_server$"] = "",
+  ["tsserver$"] = "typescript",
+  ["-tools$"] = "",
+  ["_ls$"] = "",
+  ["ls$"] = "",
+  ["_+$"] = "",
+  ["-+$"] = "",
+}
+
 M.init = function(self, options)
   M.super.init(self, options)
 end
 
 M.lsp_client_name = function()
-  local result = "[𝝺]"
+  local sign = "★"
+  local result = sign
+  local client_names = {}
 
   local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
   local clients = vim.lsp.get_active_clients()
@@ -18,8 +30,19 @@ M.lsp_client_name = function()
     local filetypes = client.config.filetypes
 
     if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-      return "[" .. client.name .. "]"
+      local name = client.name
+
+      for pattern, replacement in pairs(M.replacements) do
+        name = name:gsub(pattern, replacement)
+      end
+
+      table.insert(client_names, name)
     end
+  end
+
+  if #client_names > 0 then
+    table.sort(client_names, function(a, b) return #a < #b end)
+    result = table.concat(client_names, " | ") .. "  " .. sign
   end
 
   return result
--- readme.md
+++ readme.md
@@ -1,3 +1,35 @@
-# Lualine LSP Client Name
+# Lualine LSP Client Names
 
 Display `LSP` client name in `lualine`
+
+## Configuration
+
+```lua
+Lazy.use {
+  "nvim-lualine/lualine.nvim",
+  lazy = false,
+  priority = 512,
+  dependencies = {
+    { "yavorski/lsp-progress.nvim", config = true }, -- display lsp progress
+    { "yavorski/lualine-lsp-client-name.nvim", config = true }, -- display lsp client name
+    { "yavorski/lualine-macro-recording.nvim", config = true }, -- display macro recording
+  },
+  opts = function()
+    local catppuccin = Dark.lualine()
+
+    return {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename", "filesize", "lsp_progress", "macro_recording", "%S" },
+        lualine_x = { "selectioncount", "searchcount", "lsp_client_name", "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+      }
+    }
+  end,
+  config = function(plugin, options)
+    require("lualine").setup(options)
+  end
+}
+```
