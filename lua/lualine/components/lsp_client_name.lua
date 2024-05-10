588b59df74920c4f6a08c67e8eca2e537509b490
yavorski <yavorski.wrx@gmail.com>
2024-05-10 17:05:12 +0300

    preserve replacing order

--- lua/lualine/components/lsp_client_name.lua
+++ lua/lualine/components/lsp_client_name.lua
@@ -1,13 +1,13 @@
 local M = require("lualine.component"):extend()
 
 M.replacements = {
-  ["_language_server$"] = "",
-  ["tsserver$"] = "typescript",
-  ["-tools$"] = "",
-  ["_ls$"] = "",
-  ["ls$"] = "",
-  ["_+$"] = "",
-  ["-+$"] = "",
+  [1] = { pattern = "_language_server$", replacement = "" },
+  [2] = { pattern = "tsserver$", replacement = "typescript" },
+  [3] = { pattern = "-tools$", replacement = "" },
+  [4] = { pattern = "_ls$", replacement = "" },
+  [5] = { pattern = "ls$", replacement = "" },
+  [6] = { pattern = "_+$", replacement = "" },
+  [7] = { pattern = "-+$", replacement = "" },
 }
 
 M.init = function(self, options)
@@ -18,9 +18,8 @@ M.lsp_client_name = function()
   local sign = "★"
   local result = sign
   local client_names = {}
-
-  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
   local clients = vim.lsp.get_active_clients()
+  local buffer_filetype = vim.api.nvim_buf_get_option(0, "filetype")
 
   if next(clients) == nil then
     return result
@@ -29,11 +28,11 @@ M.lsp_client_name = function()
   for _, client in ipairs(clients) do
     local filetypes = client.config.filetypes
 
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+    if filetypes and vim.fn.index(filetypes, buffer_filetype) ~= -1 then
       local name = client.name
 
-      for pattern, replacement in pairs(M.replacements) do
-        name = name:gsub(pattern, replacement)
+      for _, entry in ipairs(M.replacements) do
+        name = name:gsub(entry.pattern, entry.replacement)
       end
 
       table.insert(client_names, name)
